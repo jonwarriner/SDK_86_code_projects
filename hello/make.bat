861d07767f92a66d289d24651d058fc7e4fa329d
Jon Warriner <jonwarriner@gmail.com>
2020-06-14 18:37:55 -0400

    clean up of make file and linker file - spaces instead of tabs - removed pipe (|) from comments
    These were causing some problems when trying to compile in freeDOS

--- hello/make.bat
+++ hello/make.bat
@@ -11,78 +11,75 @@ C:\IC86_41\IC86 Intl8251.c mod86 code searchinclude(C:\IC86_41\)
 C:\IC86_41\IC86 my_stdio.c mod86 code searchinclude(C:\IC86_41\)
 C:\IC86_41\IC86 logo.c mod86 code searchinclude(C:\IC86_41\)
 
-rem C:\IC86_41\LINK86 hello.obj,digits.obj,keys.obj,Intl8279.obj,Intl8255.obj,Intl8251.obj,msDelay.obj,cstart.obj,my_stdio.obj,logo.obj
-rem C:\IC86_41\LINK86 hello.obj,    & ; comment rem digits.obj,                     & ; comment rem keys.obj,                       & ; comment rem Intl8279.obj,                   & ; comment rem Intl8255.obj,                   & ; comment rem Intl8251.obj,                   & ; comment rem msDelay.obj,                    & ; comment rem cstart.obj,                     & ; comment rem my_stdio.obj,                   & ; comment rem logo.obj                        & ; comment rem to hello.86
-
 C:\IC86_41\LINK86 &<link.con
 
 rem RAM Memory Map 
 rem      _______________________
-rem 0H  |                       |
-rem   	|       Interrupt       |
-rem   	|      Vectors 0-4      |
-rem 13	|_______________________|
-rem 14	|                       |
-rem     |                       |
-rem     |                       |
-rem     |        Monitor        |
-rem     |         Data          |
-rem     |         Area          |
-rem     |                       |
-rem     |                       |
-rem CF  |_______________________|
-rem D0  |                       |
-rem     |                       |
-rem     |         User          |
-rem     |         Stack         |
-rem     |                       |
-rem FF  |_______________________|
-rem 100 |                       |
-rem     |       INITCODE        |
-rem     |        CSTART         |
-rem 11F |_______________________|
-rem 120 |                       |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem     |         CODE          |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem BFF |_______________________|
-rem C00 |                       |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem     |          DATA         |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem DFF |_______________________|
-rem E00 |                       |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem     |         CONST         |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem EFF |_______________________|
-rem F00 |                       |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem     |         STACK         |
-rem     |                       |
-rem     |                       |
-rem     |                       |
-rem FFF |_______________________|
+rem 0H  l                       l
+rem     l       Interrupt       l
+rem     l      Vectors 0-4      l
+rem 13  l_______________________l
+rem 14  l                       l
+rem     l                       l
+rem     l                       l
+rem     l        Monitor        l
+rem     l         Data          l
+rem     l         Area          l
+rem     l                       l
+rem     l                       l
+rem CF  l_______________________l
+rem D0  l                       l
+rem     l                       l
+rem     l         User          l
+rem     l         Stack         l
+rem     l                       l
+rem FF  l_______________________l
+rem 100 l                       l
+rem     l       INITCODE        l
+rem     l        CSTART         l
+rem 11F l_______________________l
+rem 120 l                       l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem     l         CODE          l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem BFF l_______________________l
+rem C00 l                       l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem     l          DATA         l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem DFF l_______________________l
+rem E00 l                       l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem     l         CONST         l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem EFF l_______________________l
+rem F00 l                       l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem     l         STACK         l
+rem     l                       l
+rem     l                       l
+rem     l                       l
+rem FFF l_______________________l
 
 C:\IC86_41\LOC86 hello.lnk START(MAIN) INITCODE(100H) ADDRESSES(SEGMENTS(CODE(120H),DATA(C00H),CONST(E00H),STACK(F00H))) MAP
 C:\IC86_41\OH86 hello TO hello.hex
